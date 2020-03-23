56de61db2125c2258ee430ab7a3047fe8c5a1238
Anthony Scata <anthony.scata@imaginatelabs.com>
2020-03-23 14:24:06 +1100

    Fix docker run alias with extra quote

--- cfg/aliases/aliases_docker.zsh
+++ cfg/aliases/aliases_docker.zsh
@@ -6,6 +6,6 @@ alias docker_clean_volumes='docker volume rm $(docker volume ls -qf dangling=tru
 alias docker_clean_containers='docker rm $(docker ps -qa --no-trunc --filter "status=exited")'
 
 ## RUN HELPERS
-alias dr='docker run -it --rm --volume "${PWD}":/pwd --workdir=/pwd"'
+alias dr='docker run -it --rm --volume "${PWD}":/pwd --workdir=/pwd'
 alias drs='docker run -it --rm --volume "${PWD}":/pwd --workdir=/pwd --entrypoint=/bin/sh'
 alias drb='docker run -it --rm --volume "${PWD}":/pwd --workdir=/pwd --entrypoint=/bin/bash'
