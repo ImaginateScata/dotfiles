52e015f6be5f793041cdb71915b11ffbbc2c4432
Anthony Scata <anthony.scata@imaginatelabs.com>
2020-03-23 14:25:13 +1100

    Add additional links for new files and change file permissions to match what is required

--- bin/links.sh
+++ bin/links.sh
@@ -4,12 +4,25 @@ DOTFILES="${HOME}/Code/ImaginateScata/github.com/dotfiles"
 
 echo "Setting links from '${DOTFILES}'"
 
-rm "${HOME}/Brewfile" \
-   "${HOME}/.zshrc" \
-   "${HOME}/.saml2aws"
+echo "..removing old files"
+rm -f "${HOME}/Brewfile" \
+      "${HOME}/.zshrc" \
+      "${HOME}/.saml2aws" \
+      "${HOME}/.ssh/config"
 
+echo "..creating new diretories"
+mkdir -p "${HOME}/.ssh/"
+mkdir -p "${HOME}/.aws/"
+
+echo "..linking files"
 ln -s "${DOTFILES}/cfg/Brewfile" "${HOME}/Brewfile"
 ln -s "${DOTFILES}/cfg/zshrc" "${HOME}/.zshrc"
 ln -s "${DOTFILES}/cfg/saml2aws" "${HOME}/.saml2aws"
+ln -s "${DOTFILES}/cfg/ssh_config" "${HOME}/.ssh/config"
+ln -s "${DOTFILES}/cfg/aws_config" "${HOME}/.aws/config"
 
-echo "..complete"
+echo "..change file permissions"
+chmod 0400 "${DOTFILES}/cfg/ssh_config"
+chmod 0400 "${HOME}/.ssh/anthony"*
+
+echo "complete"
