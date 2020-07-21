4a3124ce045da6b495a2cbba03f1a9d7265c236b
Anthony Scata <anthony.scata@imaginatelabs.com>
2020-07-21 13:55:39 +1000

    Update homebrew install to bash not ruby as its deprecated

--- bin/install_homebrew.sh
+++ bin/install_homebrew.sh
@@ -7,6 +7,6 @@ if command -v brew &> /dev/null; then
   echo "Homebrew already installed"
 else
   echo "Installing homebrew"
-  echo | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   echo "...complete"
 fi
